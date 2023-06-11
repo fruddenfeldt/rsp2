4cb0012193449a25232357e8d09e684c6e749133
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-06-11 22:04:13 +0200

    first iteration of button animations

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const playerPaper = document.getElementById('playerPaper')
 
+playerPaper.addEventListener('click', (e) => 
+{
+    console.log("paper!");
+    playerPaper.classList.add('paperAnimation')
+    setTimeout(() => {
+        playerPaper.classList.remove('paperAnimation')
+    }, 1000)
+}    
+)
 
-playerPaper.addEventListener('click', () => {alert("paper!")})
+const playerRock = document.getElementById('playerRock')
+playerRock.addEventListener('click', (e) => 
+{
+    console.log("rock!");
+    playerRock.classList.add('rockAnimation')
+    setTimeout(() => {
+        playerRock.classList.remove('rockAnimation')
+    }, 1000)
+}    
+)
 
+const playerScissors = document.getElementById('playerScissors')
+playerScissors.addEventListener('click', (e) => {
+    console.log("scissors");
+    playerScissors.classList.add('scissorAnimation')
+    setTimeout(() => {
+        playerScissors.classList.remove('scissorAnimation')
+    }, 1000)
+})
